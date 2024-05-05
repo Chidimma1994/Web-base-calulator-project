0904ee0a4fd2e001785bfe1b6241ab66aab0d938
Okpara Chidimma <phoebe.chidimmamars21@gmail.com>
2024-05-05 22:07:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// CALULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch (error) {
+        display.value = "Incorrect"
+    }
+
+}
